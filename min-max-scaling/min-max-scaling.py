fe7c5a52d238bcd135adf7bfad398bfeae9a276b
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2026-05-24 23:19:34 +0530

    Add solution for min-max-scaling

---
+++ min-max-scaling/min-max-scaling.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def min_max_scaling(data):
+    R, C = len(data), len(data[0])
+
+    X = np.array(data, dtype=float)
+    for j in range(C):
+        X_min, X_max = X[:, j].min(), X[:, j].max()
+        if X_min == X_max:
+            X[:, j] = np.zeros_like(X[:, j])
+            continue
+        for i in range(R):
+            X[i][j] = (X[i][j] - X_min) / (X_max - X_min)
+
+    return X.tolist()
