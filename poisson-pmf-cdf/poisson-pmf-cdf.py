a4d56c70fcd6d34254af9fe3a1521ecf311dd008
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2026-05-28 00:39:27 +0530

    Update solution for poisson-pmf-cdf

--- poisson-pmf-cdf/poisson-pmf-cdf.py
+++ poisson-pmf-cdf/poisson-pmf-cdf.py
@@ -2,10 +2,9 @@ import numpy as np
 import math
 
 def poisson_pmf_cdf(lam, k):
-    cdf: float = 0.0
+    cdf = 0
     for i in range(k):
-        cdf += (np.exp(-lam) * (lam ** i)) / math.factorial(i)
+        cdf += (np.exp(-lam) * np.power(lam, i)) / math.factorial(i)
 
-    pmf = (np.exp(-lam) * (lam ** k)) / math.factorial(k)
-    return [pmf, cdf + pmf]
-        
+    pdf = (np.exp(-lam) * np.power(lam, k)) / math.factorial(k)
+    return [pdf, pdf + cdf]
