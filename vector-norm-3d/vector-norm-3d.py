8aff3d5a79cd467d042f84dd52e90a05d65458f5
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2026-06-18 00:06:12 +0530

    Solve vector-norm-3d

--- vector-norm-3d/vector-norm-3d.py
+++ vector-norm-3d/vector-norm-3d.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 def vector_norm_3d(v):
-    v = np.array(v)
-    if v.ndim == 1:
+    v = np.asarray(v)
+    if v.ndim > 1:
+        return np.sqrt(np.power(v, 2).sum(axis=1))
+    else:
         return np.sqrt(np.power(v, 2).sum())
-
-    return np.sqrt(np.power(v, 2).sum(axis = 1 ))
