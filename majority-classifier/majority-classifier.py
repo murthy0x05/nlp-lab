9b3a04da015384967a3bd60d439db1b3c9cc2635
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2026-06-20 22:15:30 +0530

    Solve majority-classifier

--- majority-classifier/majority-classifier.py
+++ majority-classifier/majority-classifier.py
@@ -1,16 +1,6 @@
 import numpy as np
 
 def majority_classifier(y_train, X_test):
-    """
-    Predict the most frequent label in training data for all test samples.
-    """
-    y_train = np.array(y_train)
-    X_test = np.array(X_test)
-    labels, counts = np.unique(y_train, return_counts=True)
-    
-    majority_label = labels[np.argmax(counts)]
-    
-    num_test_samples = X_test.shape[0]
-    y_pred = np.full(num_test_samples, majority_label)
-    
-    return y_pred
+    majority_label = np.bincount(y_train).argmax()
+
+    return np.full(len(X_test), majority_label)
