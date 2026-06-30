e2fe0ef14e401513e2f0606e4e9eb2497b3e3a52
Pavan Kumar Murthy <144950297+murthy0x05@users.noreply.github.com>
2026-06-30 10:03:42 +0530

    Solve color-to-grayscale

--- color-to-grayscale/color-to-grayscale.py
+++ color-to-grayscale/color-to-grayscale.py
@@ -4,6 +4,6 @@ def color_to_grayscale(image):
     img_array = np.array(image)
     
     weights = np.array([0.299, 0.587, 0.114])
-    grayscale_image = np.dot(img_array[..., :3], weights)
+    grayscale_image = np.dot(img_array[...], weights)
     
     return grayscale_image.tolist()
